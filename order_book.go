c8f350546d95f3e5491459e26ad9615f72aaf95a
bartfokker <bart@publicsonar.com>
2018-04-23 23:06:16 +0200

    rename to offer

--- order_book.go
+++ order_book.go
@@ -1,28 +1,18 @@
 package cxtgo
 
-import (
-	"sync"
-)
-
-// BookOrder defines an offer from the order book
-type BookOrder struct {
-	Price  float64
-	Amount float64
+// Orderbook is a definition for an orderbook
+type Orderbook struct {
+	// Symbol() Symbol
+	// Head(n int) Summary
+	// Spread() float64
+	// Depth() Summary
+	// Error() error
 }
 
-// Bid represents a bid offer from the order book
-type Bid BookOrder
-
-// Ask represents an ask offer from the order book
-type Ask BookOrder
-
-// Orderbook is a definition for an orderbook
-type Orderbook interface {
-	Symbol() Symbol
-	Head(n int) Summary
-	Spread() float64
-	Depth() Summary
-	Error() error
+// BookOrder defines an offer from the order book
+type Offer struct {
+	Price  float64
+	Amount float64
 }
 
 // Summary defines a view of the order book.
@@ -32,17 +22,8 @@ type Summary struct {
 	Asks []Ask
 }
 
-type ConcurrentOrderbook struct {
-	Symbol
-	sync.RWMutex
-}
+// Bid represents a bid offer from the order book
+type Bid Offer
 
-func (co *ConcurrentOrderbook) Head(n int) Summary {
-	return Summary{}
-}
-func (co *ConcurrentOrderbook) Spread() float64 {
-	return -1.0
-}
-func (co *ConcurrentOrderbook) Depth() Summary {
-	return Summary{}
-}
+// Ask represents an ask offer from the order book
+type Ask Offer
