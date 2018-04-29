d50a38a8671fec3feffc2b0a1a2f3ee3b77cf863
bartfokker <bart@publicsonar.com>
2018-04-29 13:44:30 +0200

    update cxtgo documentation

--- cxt.go
+++ cxt.go
@@ -9,24 +9,31 @@ const (
 	Version = 0.1
 )
 
-// Lotter is the interface for converting amounts to lot sizes
+// Lotter is the interface for converting amounts to lot sizes.
 type Lotter interface {
+	// AmountToLots takes in an amount and convert it to a lot sized amount.
 	AmountToLots(Symbol, float64) float64
 }
 
-// PublicAPI are the public available calls for an exchange
+// PublicAPI are the public available calls for an exchange.
 type PublicAPI interface {
-	Market(ctx context.Context, symbol Symbol, params ...Params) (MarketInfo, error)
+	// Markets return the market information for an exchange.
 	Markets(ctx context.Context, params ...Params) (MarketInfos, error)
+	// Currencies return the currencies used by the exchange.
 	Currencies(ctx context.Context, params ...Params) (Currencies, error)
+	// Ticker returns the ticker information for a given symbol.
 	Ticker(ctx context.Context, symbol Symbol, params ...Params) (Ticker, error)
+	// Tickers returns all the ticker information for the given symbols.
 	Tickers(ctx context.Context, symbols []Symbol, params ...Params) (Tickers, error)
+	// Orderbook returns order book information for a given symbol.
 	OrderBook(ctx context.Context, symbol Symbol, params ...Params) (Orderbook, error)
+	// OHLCV returns the Open high low close volume infromation for a symbol.
 	OHLCV(ctx context.Context, params ...Params) error
+	// Trades return all the trades for a given symbol.
 	Trades(ctx context.Context, params ...Params) ([]Trade, error)
 }
 
-// AccountAPI are the private user api calls for an exchange
+// AccountAPI are the private user api calls for an exchange.
 type AccountAPI interface {
 	// Balances returns the balances from the exchange
 	Balances(ctx context.Context, params ...Params) (Balances, error)
@@ -44,26 +51,32 @@ type AccountAPI interface {
 	Withdraw(ctx context.Context, params ...Params) error
 }
 
-// OrderAPI are all the calls for creating updating and fetching orders
+// OrderAPI are all the calls for creating updating and fetching orders.
 type OrderAPI interface {
+	// LimitOrder creates a limit order at the exchange.
 	LimitOrder(ctx context.Context, symbol Symbol, side Side, amount, price float64, params ...Params) error
+	// MarketOrder creates an market order at the exchange.
 	MarketOrder(ctx context.Context, symbol Symbol, side Side, amount float64, params ...Params) error
+	// CancelOrder cancels the order for the given id.
 	CancelOrder(ctx context.Context, ID string, symbol *Symbol, params ...Params) error
-	CancelOrders(ctx context.Context, symbol Symbol, params ...Params) error
-	Order(ctx context.Context, params ...Params) ([]Order, error)
+	// Order returns the order information for the given order id.
+	Order(ctx context.Context, params ...Params) (Order, error)
+	// Orders returns the order information for all the orders for that symbol.
 	Orders(ctx context.Context, params ...Params) ([]Order, error)
+	// OpenOrders is like orders but only returning orders which are open.
 	OpenOrders(ctx context.Context, params ...Params) ([]Order, error)
+	// ClosedOrders is like orders but only returning orders which are closed.
 	ClosedOrders(ctx context.Context, params ...Params) ([]Order, error)
 }
 
-// StreamingAPI defines the streaming api endpoints for an exchange
+// StreamingAPI defines the streaming api endpoints for an exchange.
 type StreamingAPI interface {
 	OrderbookStreamer
 	TickerStreamer
 	TradeStreamer
 }
 
-// Exchange defines all the api calls for an exchange
+// Exchange defines all the api calls for an exchange.
 type Exchange interface {
 	Info() Base
 	Reset()
