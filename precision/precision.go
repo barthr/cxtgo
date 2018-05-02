1ff03da0dab9462847713173de38b98cf5a0eb42
bartfokker <bart@publicsonar.com>
2018-05-02 17:40:31 +0200

    added precision docs

--- precision/precision.go
+++ precision/precision.go
@@ -2,6 +2,8 @@ package precision
 
 import "strings"
 
+// FromString calculates the amount of zero's after the splitter.
+// For example 0,0001 returns the value 4.
 func FromString(input string, splitter string) int {
 	parts := strings.Split(input, splitter)
 	pricePrecision := 0
